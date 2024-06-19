a3d5073959d9479ecfc77c73a939b7dde136da3f
Deon Hill <Dehill_12@icloud.com>
2024-06-19 10:46:33 -0400

    add lombok

---
+++ src/test/java/com/Client/Client_api/lombokTest.java
@@ -0,0 +1,22 @@
+package com.Client.Client_api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.Data;
+
+public class lombokTest {
+
+    @Test
+    public void givenAnnotatedUser_thenHasGettersAndSetters() {
+        User user = new User();
+        user.setFirstName("TestFirstName");
+        assertEquals(user.getFirstName(), "TestFirstName");
+    }
+
+    @Data
+    class User {
+        private String firstName;
+    }
+}
